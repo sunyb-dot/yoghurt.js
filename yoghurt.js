2d8a6ab20620ccf78fe2ef4cdb8c730455a1a96a
mrlazy1708 <mrlazy1708@gmail.com>
2022-01-07 15:40:49 +0800

    remove unused

--- yoghurt.js
+++ yoghurt.js
@@ -34,20 +34,6 @@ window.onmousedown = function (event) {
 };
 
 /* -------------------------------------------------------------------------- */
-/*                                  UTILITIES                                 */
-/* -------------------------------------------------------------------------- */
-
-yoghurt.load = function (element, HTML) {
-  element.innerHTML = HTML; // assigned script needs a hit
-  element.querySelectorAll(`script`).forEach((oldScript) => {
-    const newScript = document.createElement(`script`);
-    Array.from(oldScript.attributes).forEach((attr) => newScript.setAttribute(attr.name, attr.value));
-    newScript.appendChild(document.createTextNode(oldScript.innerText));
-    oldScript.parentNode.replaceChild(newScript, oldScript);
-  });
-};
-
-/* -------------------------------------------------------------------------- */
 /*                                  FUNCTIONS                                 */
 /* -------------------------------------------------------------------------- */
 
